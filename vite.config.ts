2dbc76d9c9ac557cd3c5bf9ebac222d78aa3868b
xushihao <2639356786@qq.com>
2025-02-25 12:07:31 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // publicDir: '/pcdn',
-  base:'pcdn/iot/android',
+  base:'/pcdn/iot/android/',
   plugins: [react()],
   css: {
     // css模块化配置项
